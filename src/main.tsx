d6635289742dc95e29dc672a935749fab02ba20e
yb <lojobs38@gmail.com>
2023-10-19 19:59:43 +0800

    feat: add error page

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Root from "./routes/root";
+import ErrorPage from "./error-page";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 ReactDOM.createRoot(document.getElementById("root")!).render(
